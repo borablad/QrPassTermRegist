19655bfcd32d61fd63157d916e1a8aafa2064593
Bora_blad <96587810+borablad@users.noreply.github.com>
2023-01-10 14:54:14 +0600

    первичная настройка проекта

--- QrPassTermRegist/QrPassTermRegist/AppShell.xaml.cs
+++ QrPassTermRegist/QrPassTermRegist/AppShell.xaml.cs
@@ -14,6 +14,10 @@ namespace QrPassTermRegist
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
 
     }
 }
